cd57ccc95d6132b0fb1d33094120d77648d5dbb0
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-09-13 23:55:33 +0200

    Initial commit

---
+++ src/Mcip6.java
@@ -0,0 +1,29 @@
+
+
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.InetAddress;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Fernando
+ */
+public class Mcip6 {
+    /*
+    throws ClassNotFoundException, SecurityException, NoSuchMethodException, InvocationTargetException, IllegalArgumentException, IllegalAccessException
+    */
+    public static void main(String ... args) throws Exception {
+        System.setProperty("java.net.preferIPv4Stack", "false");
+        try {
+            InetAddress.getByName("::1").isReachable(1);
+        } catch (IOException ex) {
+        }
+        Class.forName("net.minecraft.client.main.Main").getDeclaredMethod("main", args.getClass()).invoke(null, (Object) args);
+    }
+}
